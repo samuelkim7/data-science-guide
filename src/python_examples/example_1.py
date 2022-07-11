e93b1c24767c20d87e13e8bcbdb3bb99da3db620
Myeongwon Kim <myeongwonkim@Myeongwons-MacBook-Pro.local>
2022-07-12 00:10:43 +0900

    python tutorials

--- src/python_examples/example_1.py
+++ src/python_examples/example_1.py
@@ -5,8 +5,22 @@
 
 
 
-# 변수
+## 변수
+# a = 1
+# b = "python"
+# c = [1,2,3]
+# print((type(a)))
+# print((type(b)))
+# print((type(c)))
 
+
+# id()
+a = [1,2,3,4,5,6,7,8,9,10]
+print(id(a))
+b = a.copy()
+print(id(b))
+b[0] = 5
+print(a, b)
 
 
 # 연산자
