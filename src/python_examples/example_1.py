ccb053be2af34d4a669a4d2e14bfe3df6c8782c1
Myeongwon Kim <myeongwonkim@Myeongwons-MacBook-Pro.local>
2022-07-20 21:51:54 +0900

    add python examples

--- src/python_examples/example_1.py
+++ src/python_examples/example_1.py
@@ -13,18 +13,30 @@
 
 
 ## 문자열
-# indexing
-
-
-# slicing
+# indexing, slicing
 
 
 # immutable / no item deletion or assignment
 # check with id()
+# a = "Pithon"
+# print(id(a))
+# a = a[:1] + 'y' + a[2:]
+# print(id(a))
+# print(a)
+# print('----------------')
+# c = "Python"
+# d = "Python"
+# print(id(c), id(d))
 
 
 # format / f-string
-
+# b = 5.72893472983478923473
+# a = "I ate {:.5f} apples".format(b)
+# c = f"I ate {b:.5f} apples"
+# print(a)
+# print(c)
+# accuracy = 0.98273647823
+# print(f"Accuracy: {accuracy:.5f}")
 
 # join / replace / split
 
@@ -33,4 +45,8 @@
 
 ## Bool
 # True of False of data types
+# def print_nums(nums, condition=None):
+#     if condition:
+#         print(nums)
 
+# print_nums([1,2,3], condition=0)
--- src/python_examples/example_2.py
+++ src/python_examples/example_2.py
@@ -3,28 +3,39 @@
 ## 리스트
 # mutable
 # 추가 / 삭제
-
+# lst = [1,2,3,4,5]
+# print(id(lst))
+# lst[1] = 10
+# print(id(lst))
 
 # indexing / slicing
 
 
 # 정렬
-
+# lst.sort()
+# lst = sorted(lst)
 
 # 추출
-
+# a = [1,2,3]
+# a.append([4])
+# a.extend(4)
+# print(a)
 
 # +, *
 
 
 ## 튜플
 # immutable / no item deletion or assignment
+a = (3,4)
+b = a * 3
+print(b)
+print(id(a), id(b))
 
 
 ## 딕셔너리
 # not sequential
 # 조회
-
+# constant time look-up
 
 # 추가
 
