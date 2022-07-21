b962907c399cbf463970ec0ab57d5f612707e4cd
Myeongwon Kim <myeongwonkim@Myeongwons-MacBook-Pro.local>
2022-07-21 09:17:56 +0900

    Add numpy example_1.py

---
+++ src/numpy_examples/example_1.py
@@ -0,0 +1,22 @@
+# list -> ndarray 생성
+# 데이터 타입을 float32로 설정
+
+import numpy as np
+
+# list -> ndarray 생성
+# 데이터 타입을 float32로 설정
+# a = np.array([1,2,3], dtype=np.float32)
+# print(a)
+
+# 차원, 각 차원의 크기, 데이터 타입 출력
+# print(a.ndim)
+# print(a.shape)
+# print(a.dtype)
+
+
+# 1로된 배열 생성 / 0으로된 배열 생성 / 0~2까지 0.2 간격의 배열 생성
+# b = np.zeros((3, 5))
+# c = np.ones((2,3))
+# print(b)
+# print(c)
+
